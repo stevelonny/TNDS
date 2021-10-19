00222714a9881790b0453b868a07d4fc326faae5
stevelonny <steve.lonny@outlook.com>
2021-10-19 19:35:58 +0200

    esercizio 3.2 with make file rooted yay

--- Lezione3/Esercizio3.2/main.cpp
+++ Lezione3/Esercizio3.2/main.cpp
@@ -2,7 +2,9 @@
 #include <fstream>
 //root
 #include "TH1F.h"
-
+#include "TApplication.h"
+#include "TGraph.h"
+#include "TCanvas.h"
 //mia libreria
 #include "stats.hpp"
 
@@ -15,11 +17,25 @@ int main(int argc, char** argv){
         return -1;
     }
 
-    int ndata = atoi(argv[1]);
-    
+    int ndata = atoi(argv[1]);    
     const char* filename = argv[2];
+
+    TApplication app("app", 0, 0);
 
     vector<double> data = Read<double>(filename, ndata);
 
-    return 0;
+    TH1F histo("histo", "histo", 100, -10, 100);
+    histo.StatOverflows(kTRUE);
+    for(int i=0; i<data.size(); i++){
+        histo.Fill(data[i]);
+    }
+
+    cout << "Media dei valori caricati: " << histo.GetMean() << endl;
+
+    TCanvas mycanvas("Histo", "Histo");
+    histo.Draw();
+    histo.GetXaxis()->SetTitle("measurement");
+
+    app.Run();
+    
 }
