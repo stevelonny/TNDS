2f2fed74a1913c7948437bdbb170bc019f1f6fc7
stevelonny <steve.lonny@outlook.com>
2022-01-09 20:44:03 +0100

    sium

--- Lezione10/graphcarlo.cpp
+++ Lezione10/graphcarlo.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include "TApplication.h"
 #include "TCanvas.h"
-#include "TH1F.h"
+#include "TH1D.h"
 #include "TAxis.h"
 #include "TLegend.h"
 #include "TImage.h"
@@ -18,15 +18,14 @@ int main(int argc, const char** argv){
     vector<unsigned int> sample_size{100, 500, 1000, 5000, 10000};
     ifstream file_in;
     ofstream file_out;
-    file_out.open("errors.dat");
+    file_out.open("errors.txt");
     string title, savename;
     vector<double> sample;
     double min{1.5}, max{2.5};
     double temp;
     int c{0};
     TCanvas can("can", "samplecan");
-    TH1F sample_hist("sample", "sample", 100, min, max);
-    sample_hist.StatOverflows(kTRUE);
+    TH1D * sample_hist;
     for(int i{0}; i<sample_size.size(); i++){
         title = fmt::format("sample_{0}.txt", sample_size[i]);
         //Lettura dati da file a vettore
@@ -44,32 +43,30 @@ int main(int argc, const char** argv){
         //Ricerca min e max per istogramma
         min = *min_element(sample.begin(), sample.end());
         max = *max_element(sample.begin(), sample.end());
+        sample_hist = new TH1D(title.c_str(), title.c_str(), 100, min, max);
         can.cd();
-        sample_hist.SetTitle(title.c_str());
-        sample_hist.SetMinimum(min);
-        sample_hist.SetMaximum(max);
+        sample_hist->SetTitle(title.c_str());
         //Riempiamo l'istogramma
         for(int j{0}; j<c; j++){
-            sample_hist.Fill(sample[i]);
+            sample_hist->Fill(sample[j]);
         }
-        fmt::print("La media del set di dati con N={1} è {0}\n", sample_hist.GetMean(), sample_size[i]);
-        fmt::print("L'errore della media {0}\n", sample_hist.GetMeanError());
-        fmt::print("La deviazione standard è {0}\n", sample_hist.GetStdDev());
-        file_out << sample_hist.GetStdDev() << endl;        
-        sample_hist.GetXaxis()->SetTitle("Valore integrale MC");
-        sample_hist.GetYaxis()->SetTitle("Frequenza");
-        sample_hist.Draw();
-        savename = fmt::format("sample_{0}.png", argv[1]);
+        fmt::print("La media del set di dati con N={1} è {0}\n", sample_hist->GetMean(), sample_size[i]);
+        fmt::print("L'errore della media {0}\n", sample_hist->GetMeanError());
+        fmt::print("La deviazione standard è {0}\n", sample_hist->GetStdDev());
+        file_out << sample_size[i] << '\t' << sample_hist->GetStdDev() << endl;        
+        sample_hist->GetXaxis()->SetTitle("Valore integrale MC");
+        sample_hist->GetYaxis()->SetTitle("Frequenza");
+        sample_hist->Draw();
+        savename = fmt::format("sample_{0}.png", sample_size[i]);
         can.Print(savename.c_str());
         //Resettiamo tutto
         file_in.close();
         can.Clear();
-        sample_hist.Reset();
-        sample_hist.ResetStats();
+        delete sample_hist;
         sample.clear();
         c=0;
     }
     file_out.close();
-    
+
     return 0;
 }
